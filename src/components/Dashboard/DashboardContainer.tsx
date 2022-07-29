2c39f223c6477321cdedc5cbd0363152d2d74ad1
Harsha Kumar <harzkr142@gmail.com>
2022-07-29 06:42:07 +0530

    Update dashboard container

--- src/components/Dashboard/DashboardContainer.tsx
+++ src/components/Dashboard/DashboardContainer.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Dashboard from "./Dashboard";
+import { useQuery } from "@tanstack/react-query";
+import { ApiResponse } from "../../utils/ApiResponse";
 
 const DashboardContainer = () => {
-    return <Dashboard />;
-}
+  const getPrograms = async () => {
+    try {
+      const response = await ApiResponse("get", "/programs");
+      return response;
+    } catch (err: any) {
+      console.log(err.data.message);
+    }
+  };
+
+  const getResidents = async () => {
+    try {
+      const response = await ApiResponse("get", "/residents");
+      return response;
+    } catch (err: any) {
+      console.log(err.data.message);
+    }
+  };
+  const programs = useQuery(["programs"], getPrograms);
+  const residents = useQuery(["residents"], getResidents);
+
+  return <Dashboard />;
+};
 
 export default DashboardContainer;
