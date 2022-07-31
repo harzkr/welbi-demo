3e50a5cdd9291411335e03cade3ed46ab8c957dc
Harsha Kumar <harzkr142@gmail.com>
2022-07-31 09:30:14 +0530

    Add filter globals

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -18,8 +18,39 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  FilterFns,
+  FilterFn
 } from "@tanstack/react-table";
+
+import {
+    RankingInfo,
+    rankItem,
+    compareItems,
+  } from '@tanstack/match-sorter-utils';
+
 import dayjs from "dayjs";
+
+declare module '@tanstack/table-core' {
+    interface FilterFns {
+      fuzzy: FilterFn<unknown>
+    }
+    interface FilterMeta {
+      itemRank: RankingInfo
+    }
+  }
+  
+  const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+    // Rank the item
+    const itemRank = rankItem(row.getValue(columnId), value)
+  
+    // Store the itemRank info
+    addMeta({
+      itemRank,
+    })
+  
+    // Return if the item should be filtered in/out
+    return itemRank.passed
+  }
 
 interface TabPanelProps {
   children?: React.ReactNode;
