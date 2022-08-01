36b30d7c05d6ce9216421f7cee9bcff8ed5cbe4c
Harsha Kumar <harzkr142@gmail.com>
2022-08-02 03:05:34 +0530

    Add new basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders submit button', () => {
   render(<App />);
   const submitElement = screen.getByText(/Submit/i);
   expect(submitElement).toBeInTheDocument();
+});
+
+test('renders email field', () => {
+  render(<App />);
+  const fieldElement = screen.getByText(/Enter your email/i);
+  expect(fieldElement).toBeInTheDocument();
 });
